9e0d634b4f5cc35c78b863a6adc5465d374e6057
Ronan Amicel <ronan.amicel@gmail.com>
2018-06-10 20:56:13 +0200

    Linux: update installed packages

--- linux/install.sh
+++ linux/install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -e
 
+# Add unofficial Sublime Text 3 installer APT repo
+sudo apt-get update
+sudo apt install --yes software-properties-common
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+
 # Install APT packages
+sudo apt-get update
 sudo apt-get install --yes $(egrep -v '^(#|$)' apt-packages-list.txt)
 
 # Install hub
