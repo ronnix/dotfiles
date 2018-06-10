3094f0954bdc3325cee90c437863d599455d0827
Ronan Amicel <ronan.amicel@gmail.com>
2018-06-10 22:04:23 +0200

    Improve osp alias

--- sublime/.bashrc.d/sublime.sh
+++ sublime/.bashrc.d/sublime.sh
@@ -2,4 +2,18 @@
 export EDITOR='subl -w'
 
 # Open Sublime Text project
-alias osp='open *.sublime-project || open ../*.sublime-project' || echo "No sublime-project file found."
+osp() {
+	d=$(pwd)
+	while ! (find "$d" -mindepth 1 -maxdepth 1 -name '*.sublime-project' | grep --quiet sublime-project) ; do
+		d=${d%/*}
+		if [ "$d" == "" ]; then break; fi
+	done
+	if [ "$d" != "" ]; then
+		for f in "$d"/*.sublime-project ; do
+			echo "Opening $f"
+			subl --project "$f"
+		done
+	else
+		echo "No *.sublime-project file found."
+	fi
+}
