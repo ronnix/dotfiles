3f09b7bc80dba81de12cbe8783b133775dce0057
Ronan Amicel <ronan.amicel@gmail.com>
2018-11-15 14:33:48 +0100

    Add bash completions for kubectl

--- bash/.bashrc.d/completions.sh
+++ bash/.bashrc.d/completions.sh
@@ -5,3 +5,10 @@ if hash brew 2>/dev/null; then
 fi
 
 export USER_BASH_COMPLETION_DIR=~/.bash_completion.d
+if [[ -d $USER_BASH_COMPLETION_DIR ]]; then
+    for i in $(LC_ALL=C command ls "$USER_BASH_COMPLETION_DIR"); do
+        i=$USER_BASH_COMPLETION_DIR/$i
+        [[ ${i##*/} != @(*~|*.bak|*.swp|\#*\#|*.dpkg*|*.rpm@(orig|new|save)|Makefile*) \
+            && -f $i && -r $i ]] && . "$i"
+    done
+fi
