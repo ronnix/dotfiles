52bf5c6941fa191acf973aa0d9231eee72909e80
Ronan Amicel <ronan.amicel@gmail.com>
2020-04-08 18:48:03 +0200

    Remove alacritty

--- install.sh.d/alacritty.sh
+++
@@ -1,6 +0,0 @@
-if [ "$(uname -s)" == "Linux" ]; then
-    sudo add-apt-repository --yes ppa:mmstick76/alacritty
-    sudo apt-get update
-    sudo apt-get install --yes alacritty
-    stow alacritty
-fi
