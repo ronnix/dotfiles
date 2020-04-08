a3566363fb795d1990f905b6cfd827b4a7618ee4
Ronan Amicel <ronan.amicel@gmail.com>
2020-04-08 19:09:02 +0200

    Add kitty terminal
    https://sw.kovidgoyal.net/kitty/

---
+++ install.sh.d/kitty.sh
@@ -0,0 +1,12 @@
+set -o errexit
+
+if [ "$(uname -s)" == "Linux" ]; then
+    version=0.17.2
+    install_dir=$HOME/.local/kitty.app
+
+    mkdir -p $install_dir
+    wget https://github.com/kovidgoyal/kitty/releases/download/v${version}/kitty-${version}-x86_64.txz -Okitty-${version}.txz
+    tar xJf kitty-${version}.txz --directory=$install_dir
+    rm -f kitty-${version}.txz
+    stow kitty
+fi
---
+++ kitty/.bashrc.d/kitty.sh
@@ -0,0 +1,4 @@
+if [ -d ~/.local/kitty.app/bin ]; then
+    export PATH=~/.local/kitty.app/bin:$PATH
+fi
+
