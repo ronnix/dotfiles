3b6cdef74188663da4351bbdf9b0b2d4da3cb3fd
Ronan Amicel <ronan.amicel@gmail.com>
2020-08-26 14:34:50 +0200

    Fix Docker install on non-Ubuntu distro

--- install.sh.d/docker.sh
+++ install.sh.d/docker.sh
@@ -1,7 +1,7 @@
 if [ "$(uname -s)" == "Linux" ]; then
     sudo apt-get install --yes apt-transport-https ca-certificates curl gnupg-agent software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
     sudo apt-get update
     sudo apt-get install --yes docker-ce docker-ce-cli containerd.io
     sudo usermod -aG docker $USER
