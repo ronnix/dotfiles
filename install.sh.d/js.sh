563b9b03b45fae876c5428efaf6294c56ad10a35
Ronan Amicel <ronan.amicel@gmail.com>
2020-08-26 14:39:15 +0200

    Improve Node setup

--- install.sh.d/js.sh
+++ install.sh.d/js.sh
@@ -1,21 +1,40 @@
 #!/bin/bash -e
 
-# See: https://yarnpkg.com/en/docs/install
-
+# 1) Install node
 if [ "$(uname -s)" == "Darwin" ]; then
     if ! brew ls --versions node >/dev/null ; then
         brew install node
     fi
+elif [ "$(uname -s)" == "Linux" ]; then
+    # See: https://github.com/nodesource/distributions#debinstall
+    curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+    sudo apt-get update
+    sudo apt-get remove --yes npm
+    sudo apt-get install --yes nodejs
+fi
+
+# Tell NPM to install global packages in my home
+mkdir -p ~/.npm-packages
+npm config set prefix ~/.npm-packages
+echo prefix=${HOME}/.npm-packages >>~/.npmrc
+
+# Install config files
+stow js
+
+# 2) Install yarn
+# See: https://yarnpkg.com/en/docs/install
+
+if [ "$(uname -s)" == "Darwin" ]; then
     if ! brew ls --versions yarn >/dev/null ; then
         brew install yarn
     fi
 elif [ "$(uname -s)" == "Linux" ]; then
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-    sudo apt-get update && sudo apt-get install --yes nodejs npm yarn
+    sudo apt-get update && sudo apt-get install --yes yarn
 fi
 
-# Prettier code formatter (https://prettier.io)
+# 3) Prettier code formatter (https://prettier.io)
 CMD="npm install --global prettier"
 if [ "$(uname -s)" == "Darwin" ]; then
     $CMD
