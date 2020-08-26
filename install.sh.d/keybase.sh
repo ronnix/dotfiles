c169056c6330acd1e00982e5863302f9a717fcd6
Ronan Amicel <ronan.amicel@gmail.com>
2020-08-26 14:36:35 +0200

    Install dependencies along with Keybase

--- install.sh.d/keybase.sh
+++ install.sh.d/keybase.sh
@@ -1,5 +1,5 @@
 if [ "$(uname -s)" == "Linux" ]; then
     curl --remote-name https://prerelease.keybase.io/keybase_amd64.deb
-    sudo apt install ./keybase_amd64.deb
+    sudo gdebi --non-interactive ./keybase_amd64.deb
     rm -f keybase_amd64.deb
 fi
