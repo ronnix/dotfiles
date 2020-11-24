5edb3c2d46ccc0a47181843bbd49c0a83666840b
Ronan Amicel <ronan.amicel@gmail.com>
2020-11-24 10:40:19 +0100

    Add Albert launcher
    https://albertlauncher.github.io/

---
+++ install.sh.d/albert.sh
@@ -0,0 +1,9 @@
+if [ "$(uname -s)" == "Linux" ]; then
+    curl "https://build.opensuse.org/projects/home:manuelschneid3r/public_key" | sudo apt-key add -
+    echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
+    curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg > /dev/null
+    sudo apt update
+    sudo apt install --yes albert
+
+    stow albert
+fi
