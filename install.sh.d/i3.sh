74a57bc1b7c7841459f07ebf1f37b8de8e8da893
Ronan Amicel <ronan.amicel@gmail.com>
2021-01-04 18:12:33 +0100

    Update picom to version 8.2

--- install.sh.d/i3.sh
+++ install.sh.d/i3.sh
@@ -26,16 +26,17 @@ if [ "$(uname -s)" == "Linux" ]; then
 
     # Install picom compositing manager
     if [ ! -f /usr/local/bin/picom ]; then
-        wget -O picom.tar.gz https://github.com/yshui/picom/archive/v8.1.tar.gz
+        VERSION=8.2
+        wget -O picom.tar.gz https://github.com/yshui/picom/archive/v$VERSION.tar.gz
         tar xzf picom.tar.gz
         rm -rf picom.tar.gz
-        pushd picom-8.1
+        pushd picom-$VERSION
         sudo apt install --yes meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev
         meson --buildtype=release . build
         ninja -C build
         sudo ninja -C build install
         popd
-        rm -rf picom-8.1
+        rm -rf picom-$VERSION
     fi
 
     stow i3
