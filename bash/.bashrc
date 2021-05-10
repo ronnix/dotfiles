0a64667cc589370682dd94071062bdb279566646
Ronan Amicel <ronan.amicel@gmail.com>
2021-05-10 10:54:16 +0200

    Install Node using nvm

--- bash/.bashrc
+++ bash/.bashrc
@@ -229,3 +229,7 @@ for script in ~/.bashrc.d/*.sh; do
         source ${script}
     fi
 done
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- install.sh.d/js.sh
+++ install.sh.d/js.sh
@@ -1,22 +1,7 @@
 #!/bin/bash -e
 
-# 1) Install node
-if [ "$(uname -s)" == "Darwin" ]; then
-    if ! brew ls --versions node >/dev/null ; then
-        brew install node
-    fi
-elif [ "$(uname -s)" == "Linux" ]; then
-    # See: https://github.com/nodesource/distributions#debinstall
-    curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-    sudo apt-get update
-    sudo apt-get remove --yes npm
-    sudo apt-get install --yes nodejs
-fi
-
-# Tell NPM to install global packages in my home
-mkdir -p ~/.npm-packages
-npm config set prefix ~/.npm-packages
-echo prefix=${HOME}/.npm-packages >>~/.npmrc
+# 1) Install node using nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 # Install config files
 stow js
@@ -35,9 +20,4 @@ elif [ "$(uname -s)" == "Linux" ]; then
 fi
 
 # 3) Prettier code formatter (https://prettier.io)
-CMD="npm install --global prettier"
-if [ "$(uname -s)" == "Darwin" ]; then
-    $CMD
-elif [ "$(uname -s)" == "Linux" ]; then
-    sudo $CMD
-fi
+npm install --global prettier
