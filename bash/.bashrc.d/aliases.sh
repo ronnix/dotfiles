11edd0090491254a338fccede1cc892f7df4e607
Ronan Amicel <ronan.amicel@gmail.com>
2021-07-20 16:54:15 +0200

    Fix ls options on macOS

--- bash/.bashrc.d/aliases.sh
+++ bash/.bashrc.d/aliases.sh
@@ -1,12 +1,16 @@
 # Colored output
-alias ls='ls --color=auto -G'
-alias ll='ls --color=auto -lhG'
+
 alias diff='colordiff'
 
-# Linux-isms
 if [ "$(uname -s)" == "Darwin" ]; then
+  alias ls='ls -G'
+  alias ll='ls -lhG'
+
   alias ldd="otool -L"
   alias rehash="set +h"
+else
+  alias ls='ls --color=auto'
+  alias ll='ls --color=auto -lh'
 fi
 
 # Explain a shell command
