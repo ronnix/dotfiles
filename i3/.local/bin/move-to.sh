f6297c9a2fcced059fd31c933f5ab483c8519c16
Ronan Amicel <ronan.amicel@gmail.com>
2021-11-15 12:07:18 +0100

    Add i3 key binding to go to selected workspace

--- i3/.local/bin/move-to.sh
+++ i3/.local/bin/move-to.sh
@@ -2,3 +2,6 @@
 WINDOW=$(i3-msg -t get_tree | jq '..|select(.focused?==true)|.id')
 WORKSPACE=$(i3-msg -t get_workspaces | jq -r .[].name | rofi -dmenu)
 i3-msg "[con_id=$WINDOW] move window to workspace $WORKSPACE"
+if [ "$1" == "--go" ]; then
+    i3-msg "workspace $WORKSPACE"
+fi
