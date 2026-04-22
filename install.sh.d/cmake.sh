578a494a7e368ee37a7ac54a5da31049dd45114a
Ronan Amicel <ronan.amicel@gmail.com>
2026-04-22 12:49:09 +0200

    Add cmake installation module

---
+++ install.sh.d/cmake.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+if [ "$(uname -s)" == "Darwin" ]; then
+    if ! brew ls --versions cmake >/dev/null ; then
+        brew install cmake
+    fi
+elif [ "$(uname -s)" == "Linux" ]; then
+    if ! dpkg -s cmake >/dev/null ; then
+        sudo apt-get install cmake
+    fi
+fi
